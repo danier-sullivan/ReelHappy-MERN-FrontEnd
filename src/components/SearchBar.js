e765f0b8d96621e84f8cdb803bef03c6dadcb146
DanGit0893 <dannysullivan300@gmail.com>
2023-04-12 15:23:56 -0500

    Moved the fetch call to the highets level so that it only has to be made once.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const SearchBar = (props) => {
-    return(
-        <div className='searchBar'>
-           <input></input>
-        
-        </div>
-    )
-   
+  return (
+    <div className='searchBar'>
+        <form>
+          <input type="text" />
+          <input type="submit" value="submit" />
+        </form>
+
+    </div>
+  )
+
 }
 
 export default SearchBar
