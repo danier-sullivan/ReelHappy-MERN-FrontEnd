1963eecc27b1440ce18ae39af09303072bccc7d8
Janinenw <janine.wexler@gmail.com>
2023-04-12 20:42:16 -0400

    top ten without fetch call

--- src/components/TopTen.js
+++ src/components/TopTen.js
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import App from '../App.js';
+import { useState, useEffect } from "react";
 
-const TopTen = ({data}) => { 
+const TopTen = ({ data }) => {
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    const sortedData = data.sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+    setMovies(sortedData.slice(0, 10));
+  }, [data]);
 
   return (
     <div>
       <h2>Top 10/ Most Recent</h2>
       <ul>
-        {data.map(movie => (
+        {movies.map((movie) => (
           <li key={movie._id}>
             <h3>{movie.title}</h3>
             <img src={movie.img} alt={movie.title} />
@@ -17,6 +23,6 @@ const TopTen = ({data}) => {
       </ul>
     </div>
   );
-        };
+};
 
 export default TopTen;
