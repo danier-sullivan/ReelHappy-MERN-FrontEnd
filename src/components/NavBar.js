9198ade10c74301ec755f22c0311d71a103488ba
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-04-12 22:33:16 -0600

    styling nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,7 @@ const NavBar = (props) => {
         <div className='nav'>
             <h1>REEL Happy</h1>
             <Link to="/"><button>HOME</button></Link>
-            <Link to="/browse"><button>BROWSE</button></Link>
-            <SearchBar/>
+            <Link to="/browse"><button>BROWSE</button></Link><SearchBar/>
         </div>
     )
 }
