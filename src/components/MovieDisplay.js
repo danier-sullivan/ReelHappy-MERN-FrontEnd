6e1d490045af853b55164965366453cb4e3928c8
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-04-14 16:11:37 -0600

    fixed errors

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import App from '../App.js';
 const MovieDisplay=({movie})=>{
     
     return(
         <div>
             <h1>{movie.title}</h1>
             <img src={movie.img} alt={movie.title}/>
-            <h2>{movie.year}</h2>
-            <h2>{movie.genre}</h2>
+            <h3>{movie.year}</h3>
+            <h3>{movie.rated}</h3>
+            <h3>{movie.genre}</h3>
+
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import SearchBar from './SearchBar'
+
 const NavBar = (props) => {
+
     return(
         <div className='nav'>
-            <h1>REEL Happy</h1>
             <Link to="/"><button>HOME</button></Link>
             <Link to="/browse"><button>BROWSE</button></Link><SearchBar/>
         </div>
