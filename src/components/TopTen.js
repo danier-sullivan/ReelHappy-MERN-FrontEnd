6ae5ab79552d39125afa2c93198f750a5e0e6439
DanGit0893 <dannysullivan300@gmail.com>
2023-04-15 12:44:16 -0500

    updated route for api connection

--- src/components/TopTen.js
+++ src/components/TopTen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import App from '../App.js';
 import {Link} from "react-router-dom"
 
-const TopTen = ({ data}) => {
+const TopTen = ({ data }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
