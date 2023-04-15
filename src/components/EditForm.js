3fbb71bdc6bb0dea1a29fdd0a5572151057e73db
markjt031 <markjt031@gmail.com>
2023-04-15 17:46:11 -0400

    broken edit form fixed

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -15,7 +15,7 @@ const [newForm, setNewForm] = useState({
 // const [comments, setComments]=useState([])
 
 const updateComment = async (comment) => {
-    await fetch(props.url+`/comments/${props.commentID}/edit`, {
+    await fetch(props.url+`/comments/${props.commentId}/edit`, {
       method: "put",
       headers: {
         "Content-Type": "application/json",
