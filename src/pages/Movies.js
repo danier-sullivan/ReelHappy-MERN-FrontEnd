6fc7786538c4996ff67e1f7a9daff59dec9970fa
Janinenw <janine.wexler@gmail.com>
2023-04-17 20:39:59 -0400

    movies on browse page display as grid

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Movies = ({ data }) => {
+  const movieListStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: '20px',
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+  };
+
+  const movieItemStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
   return (
     <div class="showAll">
       <h2>Movie Collection</h2>
-      <ul>
+      <ul style={movieListStyle}>
         {data.map((movie) => (
-          <li key={movie.id}>
+          <li key={movie.id} style={movieItemStyle}>
             <h3>{movie.title}</h3>
             <Link to={`/${movie.title}`}>
               <img src={movie.img} alt={movie.title} />
@@ -20,5 +35,3 @@ const Movies = ({ data }) => {
 };
 
 export default Movies;
-
-//comment for sake of commit
