1f0976bb7de70f854d137c9a7846c107a5128dc6
DanGit0893 <dannysullivan300@gmail.com>
2023-04-18 19:01:24 -0500

    added rough draft of the About Page

--- src/components/Explanation.js
+++ src/components/Explanation.js
@@ -1,6 +1,15 @@
 
 const Explanation = (props) => {
-    return <h1>This is the about page</h1>
+    return( 
+    <>
+    <h1>Welcome to Reel Happy!</h1> 
+
+    
+    <h3>We're a film review site with a twist: we rate movies based on how happy they make you feel. We believe that films are more than just entertainment; they're a reflection of our lives. That's why we've created a platform where you can rate and review films based on your personal definition of happiness.
+        Our community of like-minded individuals shares a passion for the joy that films can bring. You can easily search for and discover new flicks that make you feel happy, and see what others have rated them. Whether you're a cinema aficionado or just looking for a pick-me-up, Reel Happy is the perfect site for you.
+        So, come join our community of happy movie-goers today and start spreading the joy!</h3>
+    </>
+    )
 }
 
 export default Explanation;
