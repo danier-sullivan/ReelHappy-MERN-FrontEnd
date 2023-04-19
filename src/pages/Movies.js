0aca9ad0f11907a0996b5c708bfee8faadd1df85
Janinenw <janine.wexler@gmail.com>
2023-04-18 21:35:19 -0400

    code stash

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -2,30 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Movies = ({ data }) => {
-  const movieListStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '20px',
-    listStyle: 'none',
-    padding: 0,
-    margin: 0,
-  };
-
   const movieItemStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    position: 'relative',
   };
 
   return (
-    <div class="showAll">
+    <div className="showAll">
       <h2>Movie Collection</h2>
-      <ul style={movieListStyle}>
+      <ul className="movieList">
         {data.map((movie) => (
-          <li key={movie.id} style={movieItemStyle}>
-            <h3>{movie.title}</h3>
+          <li key={movie.id} style={movieItemStyle} className="movieItem">
             <Link to={`/${movie.title}`}>
-              <img src={movie.img} alt={movie.title} />
+              <div className="movieImages">
+                <img src={movie.img} alt={movie.title} />
+              </div>
             </Link>
           </li>
         ))}
